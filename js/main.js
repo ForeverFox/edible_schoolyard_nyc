31a23d85383df012c01b964a6ffe31cc44a73aa8
Forever Fox <cogden@mcad.edu>
2016-12-06 23:55:15 +0200

    Simplified navigation menu, added hamburger menu transformation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
 var navButton = document.querySelector('.nav-icon');
 navButton.addEventListener('click', function() {
   console.log('clicky!!!');
@@ -14,61 +18,4 @@ closeButton.addEventListener('click', function () {
 		.querySelector('body')
 		.classList
 		.toggle('nav-open');
-});
-
-
-var navButton = document.querySelector('.nav-icon-1');
-navButton.addEventListener('click', function() {
-  console.log('clicked');
-  document
-	  .querySelector('body')
-	  .classList
-	  .toggle('nav-open-1');
-});
-
-var closeButton = document.querySelector('.close-icon');
-closeButton.addEventListener('click', function () {
-	console.log('close it!');
-	document
-		.querySelector('body')
-		.classList
-		.toggle('nav-open-1');
-});
-
-
-var navButton = document.querySelector('.nav-icon-2');
-navButton.addEventListener('click', function() {
-  console.log('clicked');
-  document
-	  .querySelector('body')
-	  .classList
-	  .toggle('nav-open-2');
-});
-
-var closeButton = document.querySelector('.close-icon');
-closeButton.addEventListener('click', function () {
-	console.log('close it!');
-	document
-		.querySelector('body')
-		.classList
-		.toggle('nav-open-2');
-});
-
-
-var navButton = document.querySelector('.nav-icon-3');
-navButton.addEventListener('click', function() {
-  console.log('clicked');
-  document
-	  .querySelector('body')
-	  .classList
-	  .toggle('nav-open-3');
-});
-
-var closeButton = document.querySelector('.close-icon');
-closeButton.addEventListener('click', function () {
-	console.log('close it!');
-	document
-		.querySelector('body')
-		.classList
-		.toggle('nav-open-3');
 });
